17a1fa27904218b569fffc67e533b433a42e4e78
Atul Varma <varmaa@gmail.com>
2021-05-30 18:15:33 -0400

    Add more cards to sample hand.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ fn main() {
     let mut r = random::Random { seed: 15 };
     let deck = Card::new_deck();
     let hand = hand::Hand::from(vec![
+        Card { value: Value::King, suit: Suit::Diamonds },
         Card { value: Value::Ace, suit: Suit::Clubs },
+        Card { value: Value::Numeral(2), suit: Suit::Spades },
     ]);
     println!("Here's a random number: {}", r.next_float());
     println!("Here's a deck with {} cards: {:?}", deck.len(), deck);
