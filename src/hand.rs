5453e8fce92120d6a3796ddeb2b68c86333f22c5
Atul Varma <varmaa@gmail.com>
2021-05-31 09:59:04 -0400

    Add four_of_a_kind().

--- src/hand.rs
+++ src/hand.rs
@@ -7,6 +7,7 @@ use super::card::{Card, Suit, Value};
 #[derive(Debug, Eq, PartialEq)]
 pub struct Hand {
     cards: Vec<Card>,
+    grouped_by_value_lengths: Vec<(usize, Value, Vec<Card>)>,
     grouped_by_values: Vec<(Value, Vec<Card>)>,
     grouped_by_suits: Vec<(Suit, Vec<Card>)>,
 }
@@ -65,9 +66,24 @@ impl From<Vec<Card>> for Hand {
         let mut grouped_by_values = values.into_iter().collect::<Vec<_>>();
         grouped_by_values.sort_unstable_by(|(a, _), (b, _)| a.cmp(b));
 
+        let mut grouped_by_value_lengths = grouped_by_values
+            .iter()
+            .map(|(value, cards)| (cards.len(), *value, cards.clone()))
+            .collect::<Vec<_>>();
+        grouped_by_value_lengths.sort_unstable_by(|(a_len, a_val, _), (b_len, b_val, _)| {
+            let len_cmp = a_len.cmp(b_len);
+
+            if len_cmp == Ordering::Equal {
+                a_val.cmp(b_val)
+            } else {
+                len_cmp
+            }
+        });
+
         Hand {
             cards,
             grouped_by_values,
+            grouped_by_value_lengths,
             grouped_by_suits: suits.into_iter().collect::<Vec<_>>(),
         }
     }
@@ -75,11 +91,22 @@ impl From<Vec<Card>> for Hand {
 
 impl Hand {
     pub fn highest_value(&self) -> Option<Value> {
-        if self.cards.is_empty() {
-            None
+        if let Some((v, _)) = self.grouped_by_values.last() {
+            Some(*v)
         } else {
-            let (v, _) = self.grouped_by_values[self.grouped_by_values.len() - 1];
-            Some(v)
+            None
+        }
+    }
+
+    pub fn four_of_a_kind(&self) -> Option<Value> {
+        if let Some((len, v, _)) = self.grouped_by_value_lengths.last() {
+            if *len == 4 {
+                Some(*v)
+            } else {
+                None
+            }
+        } else {
+            None
         }
     }
 
@@ -123,6 +150,17 @@ mod tests {
     fn test_highest_value_works() {
         assert_eq!(hand("").highest_value(), None);
         assert_eq!(hand("2s ah 4d").highest_value(), Some(Value::Ace));
+    }
+
+    #[test]
+    fn test_four_of_a_kind_works() {
+        assert_eq!(hand("").four_of_a_kind(), None);
+        assert_eq!(hand("2s").four_of_a_kind(), None);
+        assert_eq!(hand("2s 2d 2h 2c").four_of_a_kind(), Some(Value::Two));
+        assert_eq!(
+            hand("kh 3s 3d qs 3h 4d 3c").four_of_a_kind(),
+            Some(Value::Three)
+        );
     }
 
     #[test]
